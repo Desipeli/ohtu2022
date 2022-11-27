40c1c91cf774739b7f51e8897842087f073ad5c2
Desipeli <mr.desipeli@gmail.com>
2022-11-27 19:12:52 +0200

    Yhden tuotteen lisäämisen jälkeen ostoskori sisältää yhden ostoksen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -44,3 +44,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_1_ostoksen(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(len(self.kori.ostokset()), 1)
