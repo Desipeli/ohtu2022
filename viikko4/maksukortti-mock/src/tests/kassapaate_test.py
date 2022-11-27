cc862dee293239ce18e89a3dacb701f24b9a90e2
Desipeli <mr.desipeli@gmail.com>
2022-11-27 17:43:42 +0200

    t2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_summa_pos(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 2
+
+        self.kassa.lataa(maksukortti_mock, 3)
+
+        maksukortti_mock.lataa.assert_called_with(3)
+
+    def test_lataa_summa_neg(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 2
+
+        self.kassa.lataa(maksukortti_mock, -1)
+        maksukortti_mock.lataa.assert_not_called()
