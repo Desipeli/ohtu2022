dfbd3ec5d4caa3e7e47e30ec6a35f5e63e6aa73b
Desipeli <mr.desipeli@gmail.com>
2022-12-11 13:36:07 +0200

    t3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
@@ -39,6 +39,26 @@ def main():
     for player in stats.matches(matcher):
         print(player)
 
+    print("OR")
 
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("and or")
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+    for player in stats.matches(matcher):
+        print(player)
 if __name__ == "__main__":
     main()
